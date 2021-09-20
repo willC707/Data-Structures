d666a94431a7230d21df1113fde79cd758384703
willC707 <91046237+willC707@users.noreply.github.com>
2021-09-20 04:46:09 -0400

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include "game.h"
+#include "card.h"
+#include "deck.h"
+#include "drawPile.h"
+#include "player.h"
+#include "playerHand.h"
+#include "playerStack.h"
+#include<iostream>
+
+int main()
+{
+    std::cout<< "This is working"<< std::endl;
+    return 0;
+}
---
+++ tests.cpp
@@ -0,0 +1,6 @@
+#include "game.h"
+#include "card.h"
+#include "deck.h"
+#include "drawPile.h"
+#include "player.h"
+#include <gtest/gtest.h>
